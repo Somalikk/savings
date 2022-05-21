fe479cb2af7969c5c8c471813809c72e684a1fa9
Amaefule Somtochukwu Favour <102796631+Somalikk@users.noreply.github.com>
2022-05-21 16:15:05 +0100

    Add files via upload

---
+++ css/behave.js
@@ -0,0 +1,12 @@
+document.querySelector("button").addEventListener("click", function(){
+      var rates=document.getElementById('tier3').value; 
+      if(rates==='3'){
+      				alert("Welcome");
+      }
+			else if (rates==='2'){
+							alert("Welcome ");
+			}
+			else if(rates==='1'){
+							alert("welcome ");
+			}
+});
